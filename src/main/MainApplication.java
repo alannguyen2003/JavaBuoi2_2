82fbbe82448569be61f1430053a28dfb7620b70c
alannguyen2003 <nguyenho30112003@gmail.com>
2023-05-11 23:12:29 +0700

    first commit

---
+++ src/main/MainApplication.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author acer
+ */
+public class MainApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter integer number: ");
+//        int number = sc.nextInt();
+//        System.out.println(number);
+        //Nhập các giá trị cho từng phần tử của mảng array[0] = sc.  bla bla
+//        int[] array = new int[5];
+//        Scanner sc = new Scanner(System.in);
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.print("Enter integer number at index " + i + ": ");
+//            array[i] = sc.nextInt();
+//        }
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.print(array[i] + " ");
+//        }
+        //Cũng là nhập vào các giá trị cho từng phần tử bên trong mảng,
+        //Nhưng chỉ in ra các giá trị chia hết cho giá trị x được nhập vào từ bàn phím
+        
+        /**
+         * 1. Viết chương trình tìm nghiệm của phương trình bậc nhất, với hệ số đầu vào là a và b.
+         *  ax + b = 0
+         * 2. Viết chương trình tìm nghiệm của phương trình bậc hai, với hệ số đầu vào là a, b và c.
+         *  ax^2 + bx + c = 0
+         * 3. Viết chương trình kiểm tra một số nguyên tố nhập vào từ bàn phím
+         *  Kết quả: number = 17 => true, number = 16 => false
+         * 4. Viết chương trình khai báo một mảng các số nguyên, in ra các số là số nguyên tố
+         *  VD: a = {1, 3, 7, 10, 12}
+         *  => 3, 7
+         * DEADLINE: Thứ 3, 16/5/2023
+         */
+    }
+    
+}
